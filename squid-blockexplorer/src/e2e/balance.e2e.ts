da4a2ab7c780d193a35cabaf70f90058fe1e1cb9
Serge Kovbasiuk <isserge@protonmail.com>
2023-01-12 18:21:20 +0200

    update tests

--- squid-blockexplorer/src/e2e/balance.e2e.ts
+++ squid-blockexplorer/src/e2e/balance.e2e.ts
@@ -2,19 +2,21 @@ import * as dotenv from 'dotenv';
 import tap from 'tap';
 import { GraphQLClient } from 'graphql-request';
 import { ApiPromise, WsProvider, Keyring } from '@polkadot/api';
+import type { KeyringPair } from '@polkadot/keyring/types';
 
 import { queryBalance, querySquidHeight } from './queries';
 import { submitTxAndWaitForBlockHash, wait } from './utils';
 
-dotenv.config({ path: '.env.e2e' });
+dotenv.config();
 
 const keyring = new Keyring({ type: 'sr25519', ss58Format: 2254 });
 const wsProvider = new WsProvider(process.env.CHAIN_RPC_ENDPOINT as string);
 const squidClient = new GraphQLClient(process.env.SQUID_GRAPHQL_ENDPOINT as string);
 
-console.log(`Squid GraphQL endpoint: ${process.env.SQUID_GRAPHQL_ENDPOINT}`);
-
 let rpcApi: ApiPromise;
+let ALICE: KeyringPair | undefined;
+let BOB: KeyringPair | undefined;
+const AMOUNT = 10n ** 18n;
 
 interface AccountBalance {
   total: number;
@@ -25,6 +27,23 @@ interface AccountBalance {
 
 tap.before(async () => {
   rpcApi = await ApiPromise.create({ provider: wsProvider });
+  
+  // assign account keypairs
+  ALICE = keyring.addFromUri('//Alice');
+  BOB = keyring.addFromUri('//Bob');
+
+  // send inial transfer from Alice to Bob to trigger squid indexing of ALICE and BOB accounts
+  const blockHash = await submitTxAndWaitForBlockHash(rpcApi, ALICE!, BOB!.address, AMOUNT);
+  const txBlockNumber = (await rpcApi.rpc.chain.getHeader(blockHash)).number.toNumber();
+
+  // wait for the block to be processed by the squid
+  for (; ;) {
+    // get current squid status height (latest block height)
+    const { height: squidHeight } = (await squidClient.request(querySquidHeight)).squidStatus;
+    console.log(`Waiting for the squid to catch up... ${squidHeight}/${txBlockNumber}`);
+    if (squidHeight >= txBlockNumber) break;
+    await wait(1000);
+  }
 });
 
 tap.teardown(async () => {
@@ -32,35 +51,26 @@ tap.teardown(async () => {
 });
 
 tap.test('account balances should update', async (t) => {
-  const AMOUNT = 10n ** 18n;
-
-  // get account keypairs
-  const ALICE = keyring.addFromUri('//Alice');
-  const BOB = keyring.addFromUri('//Bob');
-
   // get initial account balances from the squid
-  const { free: initAliceSquidBalance }: AccountBalance = (await squidClient.request(queryBalance, { id: ALICE.address })).accountById;
-  const { free: initBobSquidBalance }: AccountBalance = (await squidClient.request(queryBalance, { id: BOB.address })).accountById;
+  const { free: initAliceSquidBalance }: AccountBalance = (await squidClient.request(queryBalance, { id: ALICE!.address })).accountById;
+  const { free: initBobSquidBalance }: AccountBalance = (await squidClient.request(queryBalance, { id: BOB!.address })).accountById;
 
   // send a transfer from Alice to Bob
-  const blockHash = await submitTxAndWaitForBlockHash(rpcApi, ALICE, BOB.address, AMOUNT);
+  const blockHash = await submitTxAndWaitForBlockHash(rpcApi, ALICE!, BOB!.address, AMOUNT);
   const { number } = await rpcApi.rpc.chain.getHeader(blockHash);
 
   // wait for the block to be processed by the squid
   for (; ;) {
     // get current squid status height (latest block height)
     const { height: squidHeight } = (await squidClient.request(querySquidHeight)).squidStatus;
-
     console.log(`Waiting for the squid to catch up... ${squidHeight}/${number.toNumber()}`);
-
     if (squidHeight >= number.toNumber()) break;
-
     await wait(1000);
   }
 
   // get updated account balances from the squid
-  const { free: postAliceSquidBalance }: AccountBalance = (await squidClient.request(queryBalance, { id: ALICE.address })).accountById;
-  const { free: postBobSquidBalance }: AccountBalance = (await squidClient.request(queryBalance, { id: BOB.address })).accountById;
+  const { free: postAliceSquidBalance }: AccountBalance = (await squidClient.request(queryBalance, { id: ALICE!.address })).accountById;
+  const { free: postBobSquidBalance }: AccountBalance = (await squidClient.request(queryBalance, { id: BOB!.address })).accountById;
 
   // check that the balances are updated correctly
   t.equal(BigInt(initAliceSquidBalance - postAliceSquidBalance), AMOUNT);
